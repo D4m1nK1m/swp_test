04d5f9af54cbf9c880ed7fae8177e6fbd98be0e3
D4m1nK1m <adgdsda@naver.com>
2020-06-26 09:16:19 +0900

    version management start

---
+++ fig.py
@@ -0,0 +1,25 @@
+from cgi import parse_qs
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+from template import html
+
+def application(environ, start_response):
+    d = parse_qs(environ['QUERY_STRING'])
+    a = d.get('a', [''])[0]
+    b = d.get('b', [''])[0]
+    if '' not in [a, b]:
+        a, b= int(a), int(b)
+        x = a+b
+        y = a*b
+	with open('/var/www/html/img/result.txt','w')as f:
+		f.write("sum = {}, multiplied = {}".format(x,y))
+    response_body = html.replace("TEST","sum = "+str(x)+" multiplied = "+str(y))
+    
+    start_response('200 OK', [
+        ('Content-Type', 'text/html'),
+        ('Content-Length', str(len(response_body)))
+    ])
+    return [response_body]
+
+
---
+++ template.py
@@ -0,0 +1,15 @@
+html = b"""
+<html>
+    <body>
+        <form action="">
+            a = <input type="number" name="a"> <br>
+            b = <input type="number" name="b"><br><br>
+            <input type="submit">
+        </form>
+TEST
+
+    </body>
+</html>
+"""
+
+
