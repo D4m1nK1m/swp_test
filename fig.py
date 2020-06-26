a085e6e79bd8851c4a73111867192401547cfa8e
Damin Kim <adgdsda@naver.com>
2020-06-26 19:48:35 +0900

    Issue #3 fix
    when input is not digit, it returns error0 on the sum

--- fig.py
+++ fig.py
@@ -9,7 +9,11 @@ def application(environ, start_response):
     a = d.get('a', [''])[0]
     b = d.get('b', [''])[0]
     if '' not in [a, b]:
-        a, b= int(a), int(b)
+	try:
+            a, b= int(a), int(b)
+	except ValueError:
+	    a = error
+	    b = 0
     else:
 	a, b= 1,1
     x = a+b
